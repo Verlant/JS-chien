74968f004bd3ee7d9a45a0d3e105fea121aef794
Verlant <antoine.verlyck@gmail.com>
2022-12-09 09:25:57 +0100

    exo termine

--- main.js
+++ main.js
@@ -12,20 +12,34 @@ class Dog {
     this.age = age;
     this.race = race;
   }
-
+  /**
+   * renvoie un string faisant aboyer le chien
+   */
   aboie() {
     console.log(this.name + " aboie\nRex : - Waf Waf !");
   }
+  /**
+   * fonction qui donne a manger au chien et stock cette quentite dans un attribut
+   * @param {int} nbr
+   */
   mange(nbr) {
     this.quantite_nourriture += nbr;
     console.log(
-      this.name +
-        " a mangé une quantité de nourriture égale à " +
-        this.quantite_nourriture
+      this.name + " a mangé " + this.quantite_nourriture + " de nourriture."
     );
+  }
+  /**
+   * fonction qui renvoie la quantite de nourriture totale
+   * @returns valeur attribut quantite_nourriture
+   */
+  total_quantite_nourriture() {
+    return this.quantite_nourriture;
   }
 }
 const REX = new Dog("rex", 1, "berger allemand", 0);
+console.log(REX);
 REX.aboie();
 REX.mange(10);
-console.log(REX);
+REX.mange(5);
+REX.mange(2);
+REX.total_quantite_nourriture();
